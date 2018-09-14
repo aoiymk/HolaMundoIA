5672347aa62bded2a2db773b9803818c77b28b0f
Eli Carreño <smilee.season@gmail.com>
2018-09-14 11:07:15 -0300

    .

---
+++ OpenCV/cascades/cascades/muestramessi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  8 12:02:17 2018
+
+@author: gonzalofarias
+"""
+
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('messi5.jpg',0)
+
+
